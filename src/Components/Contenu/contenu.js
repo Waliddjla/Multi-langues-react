7ee789504f928b731fb83761adfe06e63cfaf92b
djellali <walidbibo1997@gmail.com>
2023-05-30 20:57:28 +0100

    first commit

---
+++ src/Components/Contenu/contenu.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react'
+import './Contenu.css'
+import data from '../../assets/data.js'
+
+export default function contenu() {
+  return (
+    <div>
+
+    </div>
+  )
+}
