ad8ccc58796c2dbc9dac37362ea52826aabd87a6
djellali <walidbibo1997@gmail.com>
2023-05-31 15:36:03 +0100

    adding default navigator lang

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -2,8 +2,18 @@ import React, { createContext, useState } from 'react';
 
 export const Context = createContext();
 
+const supportlang = ['EN', 'FR', 'AR', 'ES'];
+
+let browserlang = navigator.language.slice(0,2).toUpperCase();
+
+if (supportlang.indexOf(browserlang) === -1) {
+    console.log("not supported");
+    browserlang = 'EN';
+    
+}
+
 const ContextProvider = (props) => {
-  const [lang, setLang] = useState('EN')
+  const [lang, setLang] = useState(browserlang)
 
   const toggleLang = changLang => {
     setLang(changLang);
