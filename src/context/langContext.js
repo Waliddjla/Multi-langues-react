22778861b9aa5c6059770df9e6ac2409b132b86d
djellali <walidbibo1997@gmail.com>
2023-05-31 15:26:12 +0100

    change lang with toggleLang

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -5,8 +5,12 @@ export const Context = createContext();
 const ContextProvider = (props) => {
   const [lang, setLang] = useState('EN')
 
+  const toggleLang = changLang => {
+    setLang(changLang);
+  }
+
   return (
-    <Context.Provider value={{lang}}>
+    <Context.Provider value={{lang, toggleLang}}>
       {props.children}
     </Context.Provider>
   );
